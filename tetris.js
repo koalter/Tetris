9ce77361dd8fb83992b0b1561eabf3462c9a1af2
Lorenzo Cea Ko <38885736+koalter@users.noreply.github.com>
2018-09-26 01:28:46 -0300

    El juego ya anda

--- tetris.js
+++ tetris.js
@@ -1,30 +1,7 @@
+/* Codigo para crear la tabla */
 const canvas = document.getElementById('tetris');
 const ctx = canvas.getContext('2d');
 const sq = 20;
-
-function dibujarCuadrado(x, y, color)
-{
-    ctx.fillStyle = color;
-    ctx.fillRect(x*sq, y*sq, sq, sq);
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(x*sq, y*sq, sq, sq);
-}
-
-function dibujar(x, y, matriz, color)
-{
-    for (let r = 0; r < matriz.length; r++) 
-    {
-        for (let c = 0; c < matriz.length; c++)
-        {
-            if (matriz[r][c] == 1) 
-            {
-                dibujarCuadrado(x + c,y + r, color);
-            }
-        }
-    }
-}
-
-/* Codigo para crear la tabla */
 const filas = 20;
 const columnas = 10;
 const vacio = "white";
@@ -35,30 +12,25 @@ for (let r = 0; r < filas; r++)
     tabla[r] = [];
     for (let c = 0; c < columnas; c++) 
     {
-        dibujarCuadrado(c, r, vacio);
+        tabla[r][c] = vacio;
     }
 }
+dibujarTabla();
+// hasta aca dibujamos la tabla
 
-/* Codigo para las piezas */
-const z = [[[1, 1, 0], [0, 1, 1], [0, 0, 0]],
-            [[0, 0, 1], [0, 1, 1], [0, 1, 0]],
-            [[0, 0, 0], [1, 1, 0], [0, 1, 1]],
-            [[0, 1, 0], [1, 1, 0], [1, 0, 0]]];
-// let pieza = z[0];
-// const length = pieza.length;
-// const piezaColor = "orange";
-// for (let r = 0; r < length; r++) 
-// {
-//     for (let c = 0; c < length; c++) 
-//     {
-//         if (pieza[r][c] == 1) 
-//         {
-//             dibujarCuadrado(c, r, piezaColor);
-//         }
-//     }
-// }
+let txtPuntos = document.getElementById("puntos");
+let puntos = 0;
+txtPuntos.innerHTML = "Puntos: " + puntos;
 
-/* Creacion del objeto */
+let gameOver = false;
+let p = generarPieza();
+// instanciamos una pieza
+
+let tirar = Date.now();
+Tirar();
+// hacemos que caiga
+
+/* Objeto Pieza y funciones */
 function Pieza(tetromino, color)
 {
     this.tetromino = tetromino;
@@ -68,136 +40,247 @@ function Pieza(tetromino, color)
     this.x = 3;
     this.y = -2;
     /* Coordenadas del cuadradro */
-    dibujar(this.x, this.y, this.activeTetromino, this.color);
-
-    Pieza.prototype.dibujar = function()
+    for (let r = 0; r < this.activeTetromino.length; r++) 
     {
-        dibujar(this.x, this.y, this.activeTetromino, this.color);
-    }
-    Pieza.prototype.desDibujar = function()
-    {
-        dibujar(this.x, this.y, this.activeTetromino, vacio);
-    }
-    /* Métodos de control */
-    Pieza.prototype.rotar = function()
-    {
-        let tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
-        let siguiente = this.tetromino[tetrominoN];
-        if (this.colision(0, 0, siguiente) == false) 
+        for (let c = 0; c < this.activeTetromino.length; c++)
         {
-            this.desDibujar();
-            this.tetrominoN = tetrominoN;
-            /**
-             * 0 = (0 + 1) % 4 = 1;
-             * 1 = (1 + 1) % 4 = 2;
-             * 2 = (2 + 1) % 4 = 3;
-             * 3 = (3 + 1) % 4 = 0;
-             */
-            this.activeTetromino = siguiente;
-            this.dibujar();
-        }
-    }
-    Pieza.prototype.moverAbajo = function()
-    {
-        if (this.colision(0, 1, this.activeTetromino) == false) 
-        {
-            this.desDibujar();
-            this.y++;
-            this.dibujar();
-        }
-        else
-        {
-
-        }
-        
-    }
-    Pieza.prototype.moverIzquierda = function()
-    {
-        if (this.colision(-1, 0, this.activeTetromino) == false) 
-        {
-            this.desDibujar();
-            this.x--;
-            this.dibujar();
-        } 
-        else 
-        {
-            
-        }
-        
-    }
-    Pieza.prototype.moverDerecha = function()
-    {
-        if (this.colision(1, 0, this.activeTetromino) == false) 
-        {
-            this.desDibujar();
-            this.x++;
-            this.dibujar();
-        } 
-        else 
-        {
-            
-        }
-        
-    }
-    /* END métodos de control */
-
-    /* Prototipo para colisión */
-    Pieza.prototype.colision = function(x, y, pieza)
-    {
-        for (let r = 0; r < pieza.length; r++) 
-        {
-            for (let c = 0; c < pieza.length; c++) 
+            if (this.activeTetromino[r][c] == 1) 
             {
-                if (pieza[r][c] == 0) { continue; }
-                /*****************************************************
-                 * Las coordenadas de cualquier cuadro de un tetromino:
-                 * this.x + c
-                 * this.y + r
-                 *****************************************************/
-                /*****************************************************
-                 * Las coordenadas de cualquier cuadro de un tetromino
-                 * despues de hacer un movimiento:
-                 * this.colision(x, y, pieza);
-                 * this.x + c + x
-                 * this.y + r + y
-                 *****************************************************/
-                let nuevoX = this.x + c + x;
-                let nuevoY = this.y + r + y;
-                if (nuevoX < 0 || nuevoX >= columnas || nuevoY >= filas) 
-                {
-                    return true;
-                }
-                if (nuevoY < 0) { continue; }
-                // if (tabla[nuevoY][nuevoX] != vacio) 
-                // {
-                //     return true;
-                // }
+                dibujarCuadrado(this.x + c,this.y + r, this.color);
             }
         }
-        return false;
     }
 }
 
-let pieza = new Pieza(z, "blue");
+Pieza.prototype.llenar = function(color)
+{
+    for (let r = 0; r < this.activeTetromino.length; r++) 
+    {
+        for (let c = 0; c < this.activeTetromino.length; c++)
+        {
+            if (this.activeTetromino[r][c] == 1) 
+            {
+                dibujarCuadrado(this.x + c,this.y + r, color);
+            }
+        }
+    }
+}
+Pieza.prototype.dibujar = function()
+{
+    this.llenar(this.color);
+}
+Pieza.prototype.desDibujar = function()
+{
+    this.llenar(vacio);
+}
 
-/* Las claves para las teclas de flechas son 37, 38, 39, 40 */
+Pieza.prototype.moverAbajo = function()
+{
+    if (this.colision(0, 1, this.activeTetromino) == false)
+    {
+        this.desDibujar();
+        this.y++;
+        this.dibujar();
+    }
+    else
+    {
+        this.bloquear();
+        p = generarPieza();
+    }
+}
+Pieza.prototype.moverIzquierda = function()
+{
+    if (this.colision(-1, 0, this.activeTetromino) == false)
+    {
+        this.desDibujar();
+        this.x--;
+        this.dibujar();
+    }
+}
+Pieza.prototype.moverDerecha = function()
+{
+    if (this.colision(1, 0, this.activeTetromino) == false)
+    {
+        this.desDibujar();
+        this.x++;
+        this.dibujar();
+    }
+}
+Pieza.prototype.rotar = function()
+{
+    let siguientePatron = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
+    let moverX = 0;
+    if (this.colision(0, 0, siguientePatron) == true) 
+    {
+        if (this.x > columnas/2) 
+        {
+            moverX = -1;
+        }
+        else
+        {
+            moverX = 1;
+        }
+    }
+    if (this.colision(moverX, 0, siguientePatron) == false) 
+    {
+        this.desDibujar();
+        this.x += moverX;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
+        this.activeTetromino = this.tetromino[this.tetrominoN];
+        this.dibujar();
+    }
+}
 
-/* Códigos para teclas */
+/* Prototipo para colisión */
+Pieza.prototype.colision = function(x, y, pieza)
+{
+    let length = pieza.length;
+    for (let r = 0; r < length; r++) 
+    {
+        for (let c = 0; c < length; c++) 
+        {
+            if (pieza[r][c] == 0) { continue; }
+            /*****************************************************
+             * Las coordenadas de cualquier cuadro de un tetromino:
+             * this.x + c
+             * this.y + r
+             *****************************************************/
+            /*****************************************************
+             * Las coordenadas de cualquier cuadro de un tetromino
+             * despues de hacer un movimiento:
+             * this.colision(x, y, pieza);
+             * this.x + c + x
+             * this.y + r + y
+             *****************************************************/
+            let nuevoX = this.x + c + x;
+            let nuevoY = this.y + r + y;
+            if (nuevoX < 0 || nuevoX >= columnas || nuevoY >= filas) 
+            {
+                return true;
+            }
+            if (nuevoY < 0) { continue; }
+            if (tabla[nuevoY][nuevoX] != vacio) 
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+Pieza.prototype.bloquear = function()
+{
+    let length = this.activeTetromino.length;
+    for (let r = 0; r < length; r++) 
+    {
+        for (let c = 0; c < length; c++) 
+        {
+            if (this.activeTetromino[r][c] == 0) { continue; }
+            // aca termina el juego
+            if (this.y + r < 0) 
+            {
+                gameOver = true;
+                alert("El juego termino! \nHiciste " + puntos + " puntos.");
+                return 0;
+            }
+
+            // bloqueamos los colores de los cuadros para que coincidan con el ultimo tetromino
+            tabla[this.y + r][this.x + c] = this.color;
+
+            /* cuando completamos una fila */
+                // iteramos por todas las filas
+                for (let r = 0; r < filas; r++) 
+                {
+                    let lineaCompleta = true;
+                    // iteramos por cada columna
+                    for (let c = 0; c < columnas; c++) 
+                    {
+                        lineaCompleta = lineaCompleta && (tabla[r][c] != vacio);
+                    }
+                    // si lineaCompleta == true, la linea está completa
+                    if (lineaCompleta) 
+                    {
+                        // necesitamos mover para abajo todas las lineas que esten por encima
+                        for (let y = r; y > 1; y--) 
+                        {
+                            for (let c = 0; c < columnas; c++) 
+                            {
+                                tabla[y][c] = tabla[y - 1][c];
+                                dibujarCuadrado(c, y, tabla[y][c]);
+                            }
+                        }
+                        for (let c = 0; c < columnas; c++) 
+                        {
+                            tabla[0][c] = vacio;
+                        }
+                        puntos += 10;
+                        txtPuntos.innerHTML = "Puntos: " + puntos;
+                    }
+                }
+        }
+    }
+}
+
+/* Controles */
 document.addEventListener("keydown", function(e)
 {
-    switch (e.keyCode)
+    switch (e.keyCode) 
     {
         case 37:
-            pieza.moverIzquierda();
+            p.moverIzquierda();
             break;
         case 38:
-            pieza.rotar();
+            p.rotar();
             break;
         case 39:
-            pieza.moverDerecha();
+            p.moverDerecha();
             break;
         case 40:
-            pieza.moverAbajo();
+            p.moverAbajo();
             break;
     }
-});
+})
+/* END Controles */
+
+function dibujarTabla()
+{
+    for (let r = 0; r < filas; r++) 
+    {
+        for (let c = 0; c < columnas; c++) 
+        {
+            dibujarCuadrado(c, r, tabla[r][c]);
+        }
+    }
+}
+
+function dibujarCuadrado(x, y, color)
+{
+    ctx.fillStyle = color;
+    ctx.fillRect(x*sq, y*sq, sq, sq);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x*sq, y*sq, sq, sq);
+}
+
+function generarPieza()
+{
+    let numPieza = Math.floor(Math.random() * PIEZAS.length)
+    return new Pieza(PIEZAS[numPieza][0], PIEZAS[numPieza][1]);
+}
+
+/* Automatizamos la caida de la pieza */
+function Tirar()
+{
+    let ahora = Date.now();
+    let intervalo = ahora - tirar;
+
+    if (intervalo > 500) 
+    {
+        p.moverAbajo();
+        tirar = Date.now();
+    }
+
+    if(gameOver == false)
+    {
+        requestAnimationFrame(Tirar);
+    }
+}
